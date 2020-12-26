c0403a530f094ce3a93ab171fdd90cb574b08f43
Thibault Gagnaux <tgagnaux@gmail.com>
2020-12-26 15:54:01 +0100

    Renames `pkgs` -> `hpkgs`
    Adds a comment to make it more clear that we are here using a specific
    pkgs set from `haskell-nix`.

--- flake.nix
+++ flake.nix
@@ -7,18 +7,19 @@
   outputs = { self, nixpkgs, flake-utils, haskell-nix }:
   flake-utils.lib.eachDefaultSystem (system:
   let
-    pkgs = haskell-nix.legacyPackages.${system};
+    # use the package set from haskell-nix to avoid rebuilding ghc
+    hpkgs = haskell-nix.legacyPackages.${system};
   in
   {
-      packages.project = pkgs.haskell-nix.cabalProject {
-        src = pkgs.haskell-nix.haskellLib.cleanGit {
+      packages.project = hpkgs.haskell-nix.cabalProject {
+        src = hpkgs.haskell-nix.haskellLib.cleanGit {
           src = ./.;
           name = "mkv-tagify";
         };
         compiler-nix-name = "ghc884";
       };
       packages.shell = self.packages.${system}.project.shellFor {
-        packages   = hpkgs: with hpkgs; [ mkv-tagify ];
+        packages   = ps: with ps; [ mkv-tagify ];
         withHoogle = true;
         tools      = {
           cabal = "3.2.0.0";
