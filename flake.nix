6c32a25747f17c05e4974f19b53efdb33dcdbaee
Thibault Gagnaux <tgagnaux@gmail.com>
2021-01-03 14:14:19 +0100

    Refactors flake and adds prepares CI integration by adding the `check` attribute

--- flake.nix
+++ flake.nix
@@ -4,22 +4,27 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
 
-  outputs = { self, nixpkgs, flake-utils, haskell-nix }:
-  flake-utils.lib.eachDefaultSystem (system:
+ outputs = { self, nixpkgs, flake-utils, haskell-nix }:
   let
+    system = "x86_64-darwin";
     # use the package set from haskell-nix to avoid rebuilding ghc
     pkgs = nixpkgs.legacyPackages.${system};
     hpkgs = haskell-nix.legacyPackages.${system};
   in
   {
-      packages.project = hpkgs.haskell-nix.cabalProject {
+      project = hpkgs.haskell-nix.cabalProject {
         src = hpkgs.haskell-nix.haskellLib.cleanGit {
           src = ./.;
           name = "mkv-tagify";
         };
         compiler-nix-name = "ghc884";
       };
-      packages.shell = self.packages.${system}.project.shellFor {
+      defaultPackage.${system} = self.project.mkv-tagify.components.tests.test;
+      defaultApp.${system} = {
+        type = "app";
+        program = "${self.defaultPackage.${system}}/bin/mkv-tagify";
+      };
+      devShell.${system} = self.project.shellFor {
         packages   = ps: with ps; [ mkv-tagify ];
         withHoogle = true;
         tools      = {
@@ -29,12 +34,10 @@
         buildInputs = [ pkgs.haskell-language-server hpkgs.haskellPackages.cabal-bounds ];
         exactDeps   = true;
       };
-      defaultPackage = self.packages.${system}.project.mkv-tagify.components.exes.mkv-tagify;
-      defaultApp = {
-        type = "app";
-        program = "${self.defaultPackage.${system}}/bin/mkv-tagify";
-      };
-      devShell = self.packages.${system}.shell;
       overlay = import ./overlay.nix;
-    });
+      checks.${system} = {
+        build = self.defaultPackage.${system};
+        test = self.project.mkv-tagify.checks.test;
+      };
+    };
 }
