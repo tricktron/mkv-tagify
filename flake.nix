5a131e70d6b848aba1f52de51fb263cf9ee7e66d
Thibault Gagnaux <tgagnaux@gmail.com>
2021-01-04 17:39:30 +0100

    Pins nixpkgs to the same as haskell-nix to get a ghc884 cache hit and updates all input dependencies

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
   description = "mkv-tagify";
-
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.haskell-nix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.url     = "github:NixOS/nixpkgs/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693";
 
  outputs = { self, nixpkgs, flake-utils, haskell-nix }:
    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
   let
-    # use the package set from haskell-nix to avoid rebuilding ghc
-    pkgs = nixpkgs.legacyPackages.${system};
-    hpkgs = haskell-nix.legacyPackages.${system};
-    project = hpkgs.haskell-nix.cabalProject {
-        src = hpkgs.haskell-nix.haskellLib.cleanGit {
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ haskell-nix.overlay ];
+    };
+    project = pkgs.haskell-nix.cabalProject {
+        src = pkgs.haskell-nix.haskellLib.cleanGit {
           src = ./.;
           name = "mkv-tagify";
         };
@@ -19,24 +20,27 @@
     };
   in
   {
-      packages = {
-        mkv-tagify-lib = project.mkv-tagify.components.library;
-        mkv-tagify-test = project.mkv-tagify.checks.test;
+    packages = {
+      mkv-tagify-lib = project.mkv-tagify.components.library;
+      mkv-tagify-test = project.mkv-tagify.checks.test;
+    };
+
+    defaultPackage = self.packages.${system}.mkv-tagify-lib;
+
+    devShell = project.shellFor {
+      packages   = ps: with ps; [ mkv-tagify ];
+      withHoogle = true;
+      tools      = {
+      cabal = "3.2.0.0";
+      hlint = "3.2.3";
       };
-      defaultPackage = self.packages.${system}.mkv-tagify-lib;
-      devShell = project.shellFor {
-        packages   = ps: with ps; [ mkv-tagify ];
-        withHoogle = true;
-        tools      = {
-          cabal = "3.2.0.0";
-          hlint = "3.2.3";
-        };
-        buildInputs = [ pkgs.haskell-language-server hpkgs.haskellPackages.cabal-bounds ];
-        exactDeps   = true;
-      };
-      checks = {
-        build = self.defaultPackage.${system};
-        test = self.packages.${system}.mkv-tagify-test;
-      };
-    });
+      buildInputs = with pkgs.haskellPackages; [ haskell-language-server cabal-bounds ];
+      exactDeps   = true;
+    };
+
+    checks = {
+      build = self.defaultPackage.${system};
+      test = self.packages.${system}.mkv-tagify-test;
+    };
+  });
 }
