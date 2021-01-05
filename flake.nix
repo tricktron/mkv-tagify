953f64d0e3117d42273a577380cbe607f4df3b8e
Thibault Gagnaux <tgagnaux@gmail.com>
2021-01-05 16:13:11 +0100

    Materializes the project to check if this further speeds up the build time

--- flake.nix
+++ flake.nix
@@ -17,9 +17,17 @@
           name = "mkv-tagify";
         };
         compiler-nix-name = "ghc884";
+        index-state       = "2021-01-04T00:00:00Z";
+        materialized      = ./mkv-tagify.materialized;
+        plan-sha256       = "1apxn3cs8wfnvhmwq7262ra4pjkz77nnz8vr57ksrf0fh9673kas";
     };
   in
   {
+    # Keep this around to quickly build mkv-tagify-project.plan-nix for materialization
+    legacyPackages = {
+      mkv-tagify-project = project;
+    };
+
     packages = {
       mkv-tagify-lib = project.mkv-tagify.components.library;
       mkv-tagify-test = project.mkv-tagify.checks.test;
@@ -36,6 +44,15 @@
       };
       buildInputs = with pkgs.haskellPackages; [ haskell-language-server cabal-bounds ];
       exactDeps   = true;
+    };
+
+    apps.repl = flake-utils.lib.mkApp {
+        drv = pkgs.writeShellScriptBin "repl" ''
+          confnix=$(mktemp)
+          echo "builtins.getFlake (toString $(git rev-parse --show-toplevel))" >$confnix
+          trap "rm $confnix" EXIT
+          nix repl $confnix
+        '';
     };
 
     checks = {
---
+++ mkv-tagify.materialized/.plan.nix/mkv-tagify.nix
@@ -0,0 +1,81 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "mkv-tagify"; version = "0.1.0.1"; };
+      license = "NONE";
+      copyright = "";
+      maintainer = "tgagnaux@gmail.com";
+      author = "Thibault Gagnaux";
+      homepage = "";
+      url = "";
+      synopsis = "";
+      description = "";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" ];
+      dataDir = "";
+      dataFiles = [];
+      extraSrcFiles = [ "CHANGELOG.md" "README.md" ];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."themoviedb" or (errorHandler.buildDepError "themoviedb"))
+          (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+          (hsPkgs."text" or (errorHandler.buildDepError "text"))
+          (hsPkgs."time" or (errorHandler.buildDepError "time"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          ];
+        buildable = true;
+        modules = [ "Mkv/Utils" ];
+        hsSourceDirs = [ "src" ];
+        };
+      exes = {
+        "mkv-tagify" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."themoviedb" or (errorHandler.buildDepError "themoviedb"))
+            (hsPkgs."hxt" or (errorHandler.buildDepError "hxt"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."mkv-tagify" or (errorHandler.buildDepError "mkv-tagify"))
+            ];
+          buildable = true;
+          hsSourceDirs = [ "app" ];
+          mainPath = [ "Main.hs" ];
+          };
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-hunit" or (errorHandler.buildDepError "tasty-hunit"))
+            (hsPkgs."themoviedb" or (errorHandler.buildDepError "themoviedb"))
+            (hsPkgs."text" or (errorHandler.buildDepError "text"))
+            (hsPkgs."time" or (errorHandler.buildDepError "time"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            (hsPkgs."mkv-tagify" or (errorHandler.buildDepError "mkv-tagify"))
+            ];
+          buildable = true;
+          hsSourceDirs = [ "test" ];
+          mainPath = [ "Main.hs" ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../.; }
